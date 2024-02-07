56471337269f307f66412336ab3b5d723cc9fec2
Riku Kaartoaho <rikukaartoaho@gmail.com>
2024-02-08 00:54:52 +0200

    updated yml + footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<div class="italic text-sm font-light">
+   <h3>Developed with <span class="text-red-500">SvelteKit </span> & <span class="text-blue-400">Typescript</span></h3> 
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
     import SvelteMarkdown from "svelte-markdown";
     
     export let data: {
@@ -13,3 +14,5 @@
 <div class="leading-loose [&_a]:font-bold [&_a]:underline">
     <SvelteMarkdown source={data.about} />
 </div>
+
+<Footer/>
