47d9779321f0af01d1e6658e6e55b530837206bc
Riku Kaartoaho <rikukaartoaho@gmail.com>
2024-02-11 15:23:17 +0200

    update projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -13,7 +13,7 @@
     <div class="my-4 grid gap-2 md:grid-cols-2 lg:gap-10">
       {#each data.projects as project}
         <a href={`/projects/${convertToSlug(project.title)}/`} class="flex flex-col bg-slate-50 items-center gap-4 py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200">
-          <img src={project.thumb} alt={project.title} width="400" height="400" class="rounded-lg transition-all duration-300" style={`view-transition-name: img-${convertToSlug(project.title)}`} />
+          <img src={project.thumb} alt={project.title} width="300" height="250" class="rounded-lg transition-all duration-300" style={`view-transition-name: img-${convertToSlug(project.title)}`} />
           <div class="text-center text-sm" style={`view-transition-name: title-${convertToSlug(project.title)}`}>
             {project.title}
           </div>
